fd27ef0e3727bb1250f0d76b15866fad195b3a5b
Michael Scotello <michael.scotello@gmail.com>
2022-02-14 19:57:49 -0800

    Make check_pwd check if the pwd contains a number, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -4,12 +4,15 @@ def check_pwd(pwd):
 
     lowerCaseFlag = 0
     upperCaseFlag = 0
+    numberFlag = 0
     for char in pwd:
         if str.islower(char):
             lowerCaseFlag = 1
         if str.isupper(char):
             upperCaseFlag = 1
-    if lowerCaseFlag == 0 or upperCaseFlag == 0:
+        if str.isnumeric(char):
+            numberFlag = 1
+    if lowerCaseFlag == 0 or upperCaseFlag == 0 or numberFlag == 0:
         return False
 
     return True
