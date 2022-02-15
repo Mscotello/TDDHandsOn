70b79b75f9fbda26768eebcd20e718d278df4b01
Michael Scotello <michael.scotello@gmail.com>
2022-02-14 20:01:01 -0800

    Make check_pwd check if there is a symbol in the string, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -2,6 +2,8 @@ def check_pwd(pwd):
     if len(pwd) < 8 or len(pwd) > 20:
         return False
 
+    symbols = "~`!@#$%^&*()_+-="
+    symbolFlag = 0
     lowerCaseFlag = 0
     upperCaseFlag = 0
     numberFlag = 0
@@ -12,7 +14,9 @@ def check_pwd(pwd):
             upperCaseFlag = 1
         if str.isnumeric(char):
             numberFlag = 1
-    if lowerCaseFlag == 0 or upperCaseFlag == 0 or numberFlag == 0:
+        if char in symbols:
+            symbolFlag = 1
+    if lowerCaseFlag == 0 or upperCaseFlag == 0 or numberFlag == 0 or symbolFlag == 0:
         return False
 
     return True
