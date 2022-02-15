b290ccdbe4ce5ca432c6510d727aa00a6ef9e820
Michael Scotello <michael.scotello@gmail.com>
2022-02-14 19:50:20 -0800

    Made check_pwd check if there is lower case letter in the the pwd, it passed

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,11 @@
 def check_pwd(pwd):
     if len(pwd) < 8 or len(pwd) > 20:
         return False
+
+    lowerCaseFlag = 0
+    for char in pwd:
+        if str.islower(pwd):
+            lowerCaseFlag = 1
+    if lowerCaseFlag == 0:
+        return False
     return True
