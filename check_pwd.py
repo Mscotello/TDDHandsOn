7dbcf55768e0846980195a25e40d11874a6b0e9c
Michael Scotello <michael.scotello@gmail.com>
2022-02-14 19:45:55 -0800

    Wrote test2 to see if a string greater than 20 characters is rejected

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,4 @@
 def check_pwd(pwd):
     if len(pwd) < 8:
         return False
-    return
+    return True
--- tests.py
+++ tests.py
@@ -7,6 +7,9 @@ class Tests(unittest.TestCase):
         test_string = "abcde"
         self.assertFalse(check_pwd(test_string))
 
+    def test_if_greater_than_20_characters_then_false(self):
+        test_string = "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"
+        self.assertFalse(check_pwd(test_string))
 
 if __name__ == '__main__':
     unittest.main()
